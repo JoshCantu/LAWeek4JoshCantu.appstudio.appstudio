35bda2cf7b406c589d2ff688100b619ae4bfaef6
JoshCantu <joshua.a.cantu@gmail.com>
2021-09-15 16:03:07 -0500

    Finished LA Week 4
    Finished LA Week 4

--- forms/pets/pets.js
+++ forms/pets/pets.js
@@ -1,1 +1,7 @@
 let petNames = ["Darcy", "Jack", "Jerry", "Marty", "Vito", "Vinny", "Riley", "Saddie", "Marge", "Sam"]
+let withSal = petNames
+withSal.push("Sal")
+let withSalLength = withSal.length
+console.log(`The third pet is named ${petNames[2]}.`)
+console.log(`The array has ${withSalLength} names in it.`)
+withSal.pop()
